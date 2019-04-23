e0d691c6d5911f0dcb5aba98c32b10323f62a37e
abelmbula <abelmbula@bam.com>
2019-04-23 20:19:59 +0200

    style (general): reorganize files
    Implement new organization of files in the repo

---
+++ UI/js/signup.js
@@ -0,0 +1,20 @@
+/*document.getElementById("firstName").addEventListener("input", function(e) {
+   if (e.target.value = "") {
+      let firstNameHelp = document.getElementById("helpFirstName");
+      firstNameHelp.textContent = "This field must not be empty";
+      firstNameHelp.style.color = "red";
+   }
+   
+   console.log(e.target.value);
+});*/
+
+
+// window.console.log("rghh");
+//let firstNameHelp = document.getElementById("helpFirstName");
+/*document.getElementById("test").textContent = "This field must not be empty";
+document.getElementById("helpFirstName").style.color = "red";
+
+document.getElementById("helpFirstName").value = "bkeklel";*/
+
+let titleElts = document.getElementsByTagName("h1");
+console.log(titleElts.length);

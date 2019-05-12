c404d4454bba6a4a36decb175b0611a576032a6d
abelmbula <abelmbula@bam.com>
2019-05-12 10:35:02 +0200

    ft (API): add /POST /auth/sgnin endpoint
    [#165770148]

--- backend/app.js
+++ backend/app.js
@@ -24,20 +24,43 @@ app.post('/api/v1/auth/signup', (req, res) => {
       status: 404,
       message: 'Last name is required',
     });
-  } 
+  }
   const data = {
-        id: db.length + 1,
-        email: req.body.email,
-        firstN: req.body.firstN,
-        lastN: req.body.lastN,
-        password: 'Patie123',
-      };
-      db.push(data);
-      return res.status(200).send({
-        status: 200,
-        message: 'User created successfully',
-        data,
-      });
+    id: db.length + 1,
+    email: req.body.email,
+    firstN: req.body.firstN,
+    lastN: req.body.lastN,
+    //password: 'Patie123',
+  };
+  db.push(data);
+  return res.status(200).send({
+    status: 200,
+    message: 'User created successfully',
+    data,
+  });
+});
+
+// Sign in endpoint
+app.post('/api/v1/auth/signin', (req, res) => {
+  if (!req.body.email) {
+    return res.status(404).send({
+      status: 404,
+      message: 'Email is required',
+    });
+  } else if (!req.body.password) {
+    return res.status(404).send({
+       status: 404,
+       message: 'Password is required',
+    });
+  }  
+  const data = {
+    email: req.body.email,
+    password: 'Patie123',
+  };
+  return res.status(200).send({
+    status: 200,
+    message: 'User signed in successfully',
+  });
 });
 
 module.exports = app;

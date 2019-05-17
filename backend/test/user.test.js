9057d9707ebaec07a817717a551bf30d97e66ebf
abelmbula <abelmbula@bam.com>
2019-05-17 02:24:23 +0200

    ft (API): add user sign up test
    [#165811583]

---
+++ backend/test/user.test.js
@@ -0,0 +1,42 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+import route from '../routes/index.routes';
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+describe('User sign up', () => {
+  const data = {
+    email: 'patikav@g.cn',
+    firstN: 'Fidele',
+    lastN: 'Mbula',
+    //password: 'Patie123',
+  };
+  const endPointPost = '/api/v1/auth/signup';
+
+  it('Should return a 200 response', () => {
+    return chai.request('http://localhost:3000')
+      .post(endPointPost)
+      .send(data)
+      .then((res) => {
+        expect(res).to.have.status(200);
+      });
+  });
+  it('Should be an object with keys and values', () => {
+    return chai.request(app)
+      .post(endPointPost)
+      .send(data)
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('email');
+        expect(res.body.email).to.not.equal(null);
+        expect(res.body).to.have.property('fistName');
+        expect(res.body.firstName).to.not.equal(null);
+        expect(res.body).to.have.property('lastName');
+        expect(res.body.lastName).to.not.equal(null);
+      });
+  });
+});
